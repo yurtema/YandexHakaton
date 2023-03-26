aa8c7cedbd3bf94b0ddcedda188bdffcb123db7c
yurtema <yurtema@gmail.com>
2023-03-26 19:46:37 +0300

    pizdec

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def handler(event):
 
     # Начинать или нет? да/нет
     if event['state']['session']['state'] == 'начнем?':
-        if event['request']['original_utterance'].lower() in phrases.yes:
+        if [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.yes]:
             rgb_base = (randint(0, 255), randint(0, 255), randint(0, 255))
             rgb_dec = (randint(0, 255), randint(0, 255), randint(0, 255))
 
@@ -28,14 +28,14 @@ def handler(event):
 
             return yandex.send_text(event, choice(phrases.start_skill), {'state': 'случайный?', 'hand': filename})
 
-        elif event['request']['original_utterance'].lower() in phrases.no:
+        elif [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.no]:
             return yandex.end_session(event, choice(phrases.end_session))
 
         return yandex.send_text(event, choice(phrases.what))
 
     # Случайный или нет? да/нет
     if event['state']['session']['state'] == 'случайный?':
-        if event['request']['original_utterance'].lower() in phrases.yes:
+        if [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.yes]:
             hand = event['state']['session']['hand']
             if hand not in os.listdir('media/temp'):
                 return yandex.send_text(event, choice(phrases.wait),
@@ -43,7 +43,7 @@ def handler(event):
             return yandex.send_image(event, choice(phrases.random), [hand, ],
                                      {'state': 'еще случайный?'})
 
-        elif event['request']['original_utterance'].lower() in phrases.no:
+        elif [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.no]:
             return yandex.send_text(event, choice(phrases.random), {'state': 'цвет?'})
 
         return yandex.send_text(event, choice(phrases.what))
@@ -55,6 +55,29 @@ def handler(event):
                                     {'state': 'ждите генерации случайного изображения'})
         return yandex.send_image(event, choice(phrases.random), [hand, ],
                                  {'state': 'еще случайный?'})
+
+    if event['state']['session']['state'] == 'еще случайный?':
+        if [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.yes] is not {}:
+            rgb_base = (randint(0, 255), randint(0, 255), randint(0, 255))
+            rgb_dec = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+            dirs = os.listdir('media')
+            dirs.remove('temp')
+            dirs.remove('misc')
+            direct = choice(dirs)
+            sample = f'{direct}/{str(randint(1, len(os.listdir(f"media/{direct}")) // 3))}'
+
+            thread = threading.Thread(target=image.recolor_hand, args=(sample, rgb_base, rgb_dec))
+            thread.start()
+
+            filename = f'{sample[-1]}_{rgb_base}_{rgb_dec}.png'
+
+            return yandex.send_text(event, choice(phrases.start_skill), {'state': 'случайный?', 'hand': filename})
+
+        elif [1 for i in event['request']['original_utterance'].lower().replace(',', ' ').replace('.', ' ').split() if i in phrases.no]:
+            return yandex.end_session(event, choice(phrases.end_session))
+
+        return yandex.send_text(event, choice(phrases.what))
 
     else:
         return 'Ничерта не сработало, пишите админу. Для админа: \n' + str(event)
